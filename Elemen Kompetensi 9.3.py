b615a6889db62c2f3a629ef72dbaf356738c7680
gagahputrabangsa <91167860+gagahputrabangsa@users.noreply.github.com>
2021-11-24 15:10:50 +0700

    Add files via upload

---
+++ Elemen Kompetensi 9.3.py
@@ -0,0 +1,16 @@
+print("@@@@@   @@@@@@   @@@@@   @@@@@@   @    @")
+print("@       @    @   @       @    @   @    @")
+print("@ @@@   @@@@@@   @ @@@   @@@@@@   @@@@@@")
+print("@   @   @    @   @   @   @    @   @    @")
+print("@@@@@   @    @   @@@@@   @    @   @    @\n")
+print("ELKOM 3")
+def mengalikan(a):
+    hitungan=1
+    for n in list:
+        hitungan*=n
+    print(hitungan)
+    return hitungan
+
+list=[1,2,4,3]
+print(list)
+mengalikan(list)
