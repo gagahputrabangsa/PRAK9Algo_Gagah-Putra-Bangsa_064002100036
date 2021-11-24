e5d1547c9cf493e9941fa5764554d4990ef58f72
gagahputrabangsa <91167860+gagahputrabangsa@users.noreply.github.com>
2021-11-24 09:33:37 +0700

    Add files via upload

---
+++ Elemen Kompetensi 9.1.py
@@ -0,0 +1,13 @@
+print("@@@@@   @@@@@@   @@@@@   @@@@@@   @    @")
+print("@       @    @   @       @    @   @    @")
+print("@ @@@   @@@@@@   @ @@@   @@@@@@   @@@@@@")
+print("@   @   @    @   @   @   @    @   @    @")
+print("@@@@@   @    @   @@@@@   @    @   @    @")
+
+
+print('=========== ELKOM 1 ===========')
+list=[1,2,4,3,1,44]
+print(list)
+print('hasil reverse ke tuple')
+print(tuple(list))
+
